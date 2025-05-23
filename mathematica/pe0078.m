df0ed90a4c1ae2ac0926846aba01e22feea3e19d
hrgn <47852976+hrgnz@users.noreply.github.com>
2025-05-23 18:15:09 +0900

    update pe0078

--- mathematica/pe0078.m
+++ mathematica/pe0078.m
@@ -1,8 +1,9 @@
 Clear["Global`*"];
-RepeatedTiming[
+AbsoluteTiming[
+ pentagonal[k_] := pentagonal[k] = Quotient[k (3 k - 1), 2];
  p[0] := 1;
- p[n_ /; n < 0] := 0;
- p[n_ /; n > 0] := p[n] = Mod[Sum[(-1)^(k + 1)*
-      (p[n - PolygonalNumber[5, k]] + p[n - PolygonalNumber[5, -k]]),
-     {k, Floor[1/6 + Sqrt[1 + 24*n]/6]}], 10^6];
+ p[_?Negative] := 0;
+ p[n_] := p[n] = Mod[
+    Sum[Cos[(k + 1) Pi] (p[n - pentagonal[k]] + p[n - pentagonal[-k]]),
+     {k, 1, Quotient[1 + Sqrt[1 + 24 n], 6]}], 10^6];
  ans = NestWhile[# + 1 &, 1, p@# != 0 &]]
